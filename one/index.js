4d4da7bcd859acc9a11e18d7037babef3eeb0cb6
Phingaz <piinoya@gmail.com>
2023-09-08 14:23:42 -0300

    Updates

--- one/index.js
+++ one/index.js
@@ -8,8 +8,11 @@ const weekday = [
   "Saturday",
 ];
 
-const day = new Date().getDay();
-const dayElement = document.querySelector('[ data-testid="currentDayOfTheWeek"]');
+const day = new Date().getUTCDay();
+const dayElement = document.querySelector(
+  '[ data-testid="currentDayOfTheWeek"]'
+); 
+
 dayElement.innerText = weekday[day];
 
 const timeElement = document.querySelector('[ data-testid="currentUTCTime"]');
@@ -17,4 +20,4 @@ const timeElement = document.querySelector('[ data-testid="currentUTCTime"]');
 setInterval(() => {
   const d = Date.now();
   timeElement.innerText = d;
-}, 1000);
+}, 100);
