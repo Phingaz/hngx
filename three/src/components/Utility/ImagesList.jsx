99614ac2f04ca14d4735a2c47e3527c24406b833
Phingaz <piinoya@gmail.com>
2023-09-19 01:21:01 -0300

    Updates

--- three/src/components/Utility/ImagesList.jsx
+++ three/src/components/Utility/ImagesList.jsx
@@ -19,7 +19,7 @@ export const ImagesList = ({images}) => {
       {items?.map((item) => (
         <SortableItem key={item.id}>
           <div
-            className={`bg-stone-100 cursor-grab select-none flex flex-col gap-2 items-center p-3 rounded-xl b min-h-[300px]`}
+            className={`bg-stone-100 cursor-grab select-none flex flex-col gap-2 items-center p-3 rounded-xl b`}
           >
             <img
               src={item.url}
